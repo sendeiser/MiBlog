4e216b30a561366a4bfca894429ffaae72f79c7b
sendeiser <87587210+sendeiser@users.noreply.github.com>
2022-03-24 19:02:39 -0300

    subida de archivos

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const typed = new Typed('.typed',{
+   strings : ["Conoce las novedades y noticias del Mundo Tech"],
+   //stringsElement: '#cadenas-texto', // ID del elemento que contiene cadenas de texto a mostrar.
+	typeSpeed: 80, // Velocidad en mlisegundos para poner una letra,
+	startDelay: 1100, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
+	backSpeed: 55, // Velocidad en milisegundos para borrrar una letra,
+	smartBackspace: false, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
+	shuffle: false, // Alterar el orden en el que escribe las palabras.
+	backDelay: 1500, // Tiempo de espera despues de que termina de escribir una palabra.
+	loop: true, // Repetir el array de strings
+	loopCount: false, // Cantidad de veces a repetir el array.  false = infinite
+	showCursor: true, // Mostrar cursor palpitanto
+	cursorChar: '|', // Caracter para el cursor
+	contentType: null, // 'html' o 'null' para texto sin formato
+})
